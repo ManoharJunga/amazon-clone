678cf97442157f5efd7edc62c2b79dad78fdb25c
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-07-10 11:26:42 +0530

    commit welcome bar

---
+++ client/SpotIt/Components/WelcomeBar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+function WelcomeBar() {
+  return (
+    <div style={styles.bar}>
+      <div style={styles.logoContainer}>
+        Welcome to SpotIt
+      </div>
+      <div style={styles.spacer} />
+      <div style={styles.right}>
+        <LocationOnIcon style={styles.icon} fontSize="small" htmlColor="#3300FF" />
+        Location
+      </div>
+      <div style={styles.verticalLine}></div>
+      <div style={styles.right}>
+        <LocalOfferIcon style={styles.icon} fontSize="small" htmlColor="#3300FF" />
+        All Offers
+      </div>
+    </div>
+  );
+}
+
+export default WelcomeBar;
+
+const styles = {
+  bar: {
+    fontSize: '13px',
+    display: 'flex',
+    color: '#666666',
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    padding: 10,
+    paddingLeft: 150,
+    paddingRight: 150,
+  },
+  logoContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  spacer: {
+    flex: 1,
+  },
+  right: {
+    marginLeft: 16,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  verticalLine: {
+    height: '30px',
+    width: 1.5,
+    backgroundColor: '#D9D9D9',
+    marginLeft: 16,
+  },
+  icon: {
+    marginRight: '5px',
+  },
+};
---
+++ client/SpotIt/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeBar from '../Components/WelcomeBar'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <WelcomeBar />
+    </>
+  )
+}
+
+export default App
