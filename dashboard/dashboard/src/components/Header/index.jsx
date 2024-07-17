3ed2c4d7f43c9e38284cbcaa020d4588d5ec8303
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-07-17 22:59:10 +0530

    commit

--- dashboard/dashboard/src/components/Header/index.jsx
+++ dashboard/dashboard/src/components/Header/index.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/Logo.svg';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import Button from '@mui/material/Button'; 
+import './Header.css';
+
 function Header() {
     return (
-        <header >
+        <header>
             <div className="container-fluid">
-                <div className="row" >
-                    {/* Logo Wraooer */}
-                    <div className="col-xs-3">
-                        <Link to={'/'}></Link>
-                    </ div>
+                <div className="row align-items-center">
+                    {/* Logo Wrapper */}
+                    <div className="col-6 col-md-3">
+                        <Link to={'/'}>
+                            <img src={logo} className='logo' alt="Logo" />
+                        </Link>
+                    </div>
+                    {/* Button Wrapper */}
+                    <div className="col-6 col-md-3 d-flex justify-content-end">
+                        <Button><MenuOpenIcon /></Button>
+                    </div>
                 </div>
             </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
